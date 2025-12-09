536d3f0f7127cf5e0182c36eebad5846827a4d4d
SARAVN <saravn.ent@gmail.com>
2025-12-10 01:02:37 +0530

    feat: configure security headers in Next.js application.

--- next.config.ts
+++ next.config.ts
@@ -44,6 +44,47 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+},
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self'; " +
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline'; " +
+              "style-src 'self' 'unsafe-inline'; " +
+              "img-src 'self' blob: data: https:; " +
+              "media-src 'self' blob: data: https:; " +
+              "connect-src 'self' https:; " +
+              "font-src 'self' data:;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withPWA(nextConfig);
