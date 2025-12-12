de7e0aafc7f1f420259240bfbb6b35a871060c93
SARAVN <saravn.ent@gmail.com>
2025-12-13 01:42:16 +0530

    image update

--- lib/manual_artists.ts
+++ lib/manual_artists.ts
@@ -1,9 +1,46 @@
 export const MANUAL_ARTIST_IMAGES: Record<string, string> = {
-    // Example Format:
-    // "Artist Name": "https://example.com/image.jpg",
-
-    // Add your manual images here:
+    // Music Directors
     "Yuvanshankar Raja": "https://upload.wikimedia.org/wikipedia/commons/e/e6/Yuvan_Shankar_Raja_at_Hyderbad_Concert.jpg",
+    "Yuvan Shankar Raja": "https://upload.wikimedia.org/wikipedia/commons/e/e6/Yuvan_Shankar_Raja_at_Hyderbad_Concert.jpg",
+    "Yuvan": "https://upload.wikimedia.org/wikipedia/commons/e/e6/Yuvan_Shankar_Raja_at_Hyderbad_Concert.jpg",
+
     "Anirudh Ravichander": "https://upload.wikimedia.org/wikipedia/commons/2/23/Anirudh_Ravichander_at_Audio_Launch.jpg",
-    // "Sid Sriram": "...",
+    "Anirudh": "https://upload.wikimedia.org/wikipedia/commons/2/23/Anirudh_Ravichander_at_Audio_Launch.jpg",
+
+    "A. R. Rahman": "https://upload.wikimedia.org/wikipedia/commons/7/70/A_R_Rahman_snapped_at_the_special_screening_of_Amar_Singh_Chamkila.jpg",
+    "A.R. Rahman": "https://upload.wikimedia.org/wikipedia/commons/7/70/A_R_Rahman_snapped_at_the_special_screening_of_Amar_Singh_Chamkila.jpg",
+    "AR Rahman": "https://upload.wikimedia.org/wikipedia/commons/7/70/A_R_Rahman_snapped_at_the_special_screening_of_Amar_Singh_Chamkila.jpg",
+    "Rahman": "https://upload.wikimedia.org/wikipedia/commons/7/70/A_R_Rahman_snapped_at_the_special_screening_of_Amar_Singh_Chamkila.jpg",
+
+    "Harris Jayaraj": "https://upload.wikimedia.org/wikipedia/commons/2/20/Harris_Jayaraj.jpg",
+    "Harris": "https://upload.wikimedia.org/wikipedia/commons/2/20/Harris_Jayaraj.jpg",
+
+    "G. V. Prakash Kumar": "https://upload.wikimedia.org/wikipedia/commons/6/67/GV_Prakash_Kumar_at_Komban_Success_Meet.jpg",
+    "G.V. Prakash Kumar": "https://upload.wikimedia.org/wikipedia/commons/6/67/GV_Prakash_Kumar_at_Komban_Success_Meet.jpg",
+    "GV Prakash": "https://upload.wikimedia.org/wikipedia/commons/6/67/GV_Prakash_Kumar_at_Komban_Success_Meet.jpg",
+    "G.V. Prakash": "https://upload.wikimedia.org/wikipedia/commons/6/67/GV_Prakash_Kumar_at_Komban_Success_Meet.jpg",
+
+    "Santhosh Narayanan": "https://upload.wikimedia.org/wikipedia/commons/5/5a/Santhosh_Narayanan_and_his_Wife_at_%27Kadugu%27_Movie_Premiere_%28cropped%29.jpg",
+    "SaNa": "https://upload.wikimedia.org/wikipedia/commons/5/5a/Santhosh_Narayanan_and_his_Wife_at_%27Kadugu%27_Movie_Premiere_%28cropped%29.jpg",
+
+    "Ilaiyaraaja": "https://upload.wikimedia.org/wikipedia/commons/1/11/Ilaiyaraaja_at_Shamitabh_Press_Show.jpg",
+    "Ilayaraja": "https://upload.wikimedia.org/wikipedia/commons/1/11/Ilaiyaraaja_at_Shamitabh_Press_Show.jpg",
+    "Isaignani": "https://upload.wikimedia.org/wikipedia/commons/1/11/Ilaiyaraaja_at_Shamitabh_Press_Show.jpg",
+
+    "Hiphop Tamizha": "https://upload.wikimedia.org/wikipedia/commons/5/52/Hiphop_Tamizha_at_Aambala_Audio_Launch.jpg",
+    "Adhi": "https://upload.wikimedia.org/wikipedia/commons/5/52/Hiphop_Tamizha_at_Aambala_Audio_Launch.jpg",
+
+    "Vidyasagar": "https://upload.wikimedia.org/wikipedia/commons/5/5e/VidyaSagar.jpg",
+
+    "D. Imman": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/D._Imman_at_Mylapore_%28cropped%29.jpg/362px-D._Imman_at_Mylapore_%28cropped%29.jpg",
+    "Imman": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/D._Imman_at_Mylapore_%28cropped%29.jpg/362px-D._Imman_at_Mylapore_%28cropped%29.jpg",
+
+    "Thaman S": "https://upload.wikimedia.org/wikipedia/commons/4/45/Thaman_at_Takkar_Pre_Release_Press_Meet.jpg",
+    "S. Thaman": "https://upload.wikimedia.org/wikipedia/commons/4/45/Thaman_at_Takkar_Pre_Release_Press_Meet.jpg",
+    "Thaman": "https://upload.wikimedia.org/wikipedia/commons/4/45/Thaman_at_Takkar_Pre_Release_Press_Meet.jpg",
+
+    "Sam C. S.": "https://upload.wikimedia.org/wikipedia/commons/7/7b/Sam_CS_at_NOTA_Press_Meet.jpg",
+    "Sam CS": "https://upload.wikimedia.org/wikipedia/commons/7/7b/Sam_CS_at_NOTA_Press_Meet.jpg",
+
+    "Ghibran": "https://upload.wikimedia.org/wikipedia/commons/8/85/Ghibran_at_Ungal_Naan_Event.jpg",
 };
